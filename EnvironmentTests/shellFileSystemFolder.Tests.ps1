e328a4cea4699a58bba866c76e068e4973f0cf7c
alx9r <eggy.island9@gmail.com>
2016-12-01 21:46:05 -0800

    add shellFileSystemFolder.Tests.ps1 test index past end test retrieval test folder order test reordering

---
+++ EnvironmentTests/shellFileSystemFolder.Tests.ps1
@@ -0,0 +1,50 @@
+Describe 'ShellFileSystemFolder' {
+    Context 'create folder' {
+        $existsPath = "$([System.IO.Path]::GetTempPath())Exists-c04c4e56"
+        It 'create a temp folder' {
+            New-Item $existsPath -ItemType Directory -Force
+        }
+        foreach ( $values in @(
+                #  throws | path
+                @( $false,  [System.IO.Path]::GetTempPath() ),
+                @( $true,  "$([System.IO.Path]::GetTempPath())NotExists-c04c4e56" ),
+                @( $false, $existsPath )
+            )
+        )
+        {
+            $throws, $path = $values
+            if ( $throws )
+            {
+                It "$path throws" {
+                    try
+                    {
+                        { $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($path) } |
+                            Should throw
+                    }
+                    finally
+                    {
+                        if ( $null -ne $f )
+                        {
+                            $f.Dispose()
+                        }
+                    }
+                }
+                continue
+            }
+            It "$path succeeds" {
+                try
+                {
+                    $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($path)
+                    $f.Path | Should be $path
+                }
+                finally
+                {
+                    if ( $null -ne $f )
+                    {
+                        $f.Dispose()
+                    }
+                }
+            }
+        }
+    }
+}
--- EnvironmentTests/shellLibraryFolder.Tests.ps1
+++ EnvironmentTests/shellLibraryFolder.Tests.ps1
@@ -101,6 +101,19 @@ Describe 'ShellLibrary folder' {
                 $l.Dispose()
             }
         }
+        It '.Item() throws for index past end' {
+            try
+            {
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                { $f = $l.Item(1) } |
+                    Should throw 'Index was out of range'
+            }
+            finally
+            {
+                if ( $f ) { $f.Dispose() }
+                if ( $f ) { $l.Dispose() }
+            }
+        }
         It 'remove the folder' {
             try
             {
@@ -154,6 +167,221 @@ Describe 'ShellLibrary folder' {
             finally
             {
                 $f.Dispose()
+                $l.Dispose()
+            }
+        }
+    }
+
+    foreach ( $values in @(
+            # indexOfSucceeds | path
+            @( $true, 'c:\temp' ),
+            @( $false, [System.IO.Path]::GetTempPath() ),
+            @( $false, $env:APPDATA ),
+            @( $true, $env:ProgramData ),
+            @( $false, $env:USERPROFILE ),
+            @( $false, "$env:USERPROFILE\Folder" ),
+            @( $true, "$env:USERPROFILE\Documents" )
+        )
+    )
+    {
+        $indexOfSucceeds,$path = $values
+        $folderPath = Join-Path $path 'Folder-5990a4a9'
+        Context "add and retrieve folder $folderPath" {
+            It 'create folder' {
+                New-Item $folderPath -ItemType Directory -Force
+            }
+            It 'add a folder to library' {
+                try
+                {
+                    $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folderPath)
+                    $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$true)
+                    $l.Add($f)
+                }
+                finally
+                {
+                    if ( $null -ne $f ) { $f.Dispose() }
+                    if ( $null -ne $l ) { $l.Dispose() }
+                }
+            }
+            It '.Contains() succeeds' {
+                try
+                {
+                    $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folderPath)
+                    $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                    $r = $l.Contains($f)
+                    $r | Should be $true
+                }
+                finally
+                {
+                    $f.Dispose()
+                    $l.Dispose()
+                }
+            }
+            if ( $indexOfSucceeds )
+            {
+                It '.IndexOf() retrieving the folder from library succeeds' {
+                    try
+                    {
+                        $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folderPath)
+                        $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                        $i = $l.IndexOf($f)
+                        $i | Should be 0
+                        $r = $l.Item($i)
+                        $r.Path | Should be $folderPath
+                    }
+                    finally
+                    {
+                        $f.Dispose()
+                        $l.Dispose()
+                    }
+                }
+            }
+            else
+            {
+                It 'retrieving the folder from library fails' {
+                    try
+                    {
+                        $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folderPath)
+                        $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                        $i = $l.IndexOf($f)
+                        $i | Should be -1
+                    }
+                    finally
+                    {
+                        $f.Dispose()
+                        $l.Dispose()
+                    }
+                }
+            }
+            It 'search succeeds' {
+                try
+                {
+                    $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                    $r = $l | ? { $_.Path -eq $folderPath }
+                    $r.Path | Should be $folderPath
+                }
+                finally
+                {
+                    $l.Dispose()
+                    $r.Dispose()
+                }                
+            }
+        }
+    }
+    Context 'folder order' {
+        $tempPath = [System.IO.Path]::GetTempPath()
+        $folder1Path = Join-path $tempPath "Folder1-a2cce939"
+        $folder2Path = Join-path $tempPath "Folder2-a2cce939"
+        It 'create folders' {
+            New-Item $folder1Path,$folder2Path -ItemType Directory -Force
+        }
+        It 'add folders to library in order' {
+            try
+            {
+                $f1 = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folder1Path)
+                $f2 = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folder2Path)
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$true)
+                $l.Add($f1)
+                $l.Add($f2)
+            }
+            finally
+            {
+                $f1.Dispose()
+                $f2.Dispose()
+                $l.Dispose()
+            }
+        }
+        It 'first folder is first, second folder is second' {
+            try
+            {
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                $l.Item(0).Path | Should be $folder1Path
+                $l.Item(1).Path | Should be $folder2Path
+            }
+            finally
+            {
+                $l.Dispose()
+            }
+        }
+        It 'add folders to library in opposite order' {
+            try
+            {
+                $f1 = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folder1Path)
+                $f2 = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folder2Path)
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$true)
+                $l.Add($f2)
+                $l.Add($f1)
+            }
+            finally
+            {
+                $f1.Dispose()
+                $f2.Dispose()
+                $l.Dispose()
+            }
+        }
+        It 'first folder is second, second folder is first' {
+            try
+            {
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                $l.Item(0).Path | Should be $folder2Path
+                $l.Item(1).Path | Should be $folder1Path
+            }
+            finally
+            {
+                $l.Dispose()
+            }
+        }
+    }
+    Context 'reorder' {
+        $tempPath = [System.IO.Path]::GetTempPath()
+        $folder1Path = Join-path $tempPath "Folder1-a2cce939"
+        $folder2Path = Join-path $tempPath "Folder2-a2cce939"
+        It 'create folders' {
+            New-Item $folder1Path,$folder2Path -ItemType Directory -Force
+        }
+        It 'add folders to library in order' {
+            try
+            {
+                $f1 = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folder1Path)
+                $f2 = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folder2Path)
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$true)
+                $l.Add($f1)
+                $l.Add($f2)
+            }
+            finally
+            {
+                $f1.Dispose()
+                $f2.Dispose()
+                $l.Dispose()
+            }
+        }
+        It 'reverse the order' {
+            $stack = [System.Collections.Stack]::new()
+            try
+            {
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                $l | % { $stack.Push($_) }
+                $l.Dispose()
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$true)
+                $stack | % { 
+                    $l.Add($_)
+                    $_.Dispose()
+                }
+            }
+            finally
+            {
+                $l.Dispose()
+            }
+        }
+        It 'first folder is second, second folder is first' {
+            try
+            {
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                $l.Item(0).Path | Should be $folder2Path
+                $l.Item(1).Path | Should be $folder1Path
+            }
+            finally
+            {
                 $l.Dispose()
             }
         }
