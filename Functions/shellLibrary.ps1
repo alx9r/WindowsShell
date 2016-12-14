b5a63ed243f648470d2a2a04c37e8f3e97fed3ff
alx9r <eggy.island9@gmail.com>
2016-12-14 13:28:33 -0800

    implement Add-ShellLibrary

--- Functions/shellLibrary.ps1
+++ Functions/shellLibrary.ps1
@@ -1,3 +1,59 @@
+function Get-ShellLibraryPath
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   ValueFromPipeline = $true)]
+        [string]
+        $Name
+    )
+    process
+    {
+        $librariesPath = [System.IO.Path]::Combine(
+            [System.Environment]::GetFolderPath( [System.Environment+SpecialFolder]::ApplicationData ),
+            [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::LibrariesKnownFolder.RelativePath
+        )
+        $libraryPath = [System.IO.Path]::Combine($librariesPath, $Name)
+        return [System.IO.Path]::ChangeExtension($libraryPath, "library-ms")
+    }    
+}
+
+function Test-ShellLibrary
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   ValueFromPipeline = $true)]
+        [string]
+        $Name
+    )
+    process
+    {
+        $Name | Get-ShellLibraryPath | Test-Path -ea Stop
+    }
+}
+
+function Convert-ShellLibraryObject
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   Position = 1)]
+        $InputObject
+    )
+    process
+    {
+        New-Object ShellLibrary -Property @{
+            Name     = $InputObject.Name
+            TypeName = $InputObject.LibraryType
+            IconReferencePath = $InputObject.IconResourceId.ReferencePath
+        }
+    }
+}
+
 function Get-ShellLibrary
 {
     [CmdletBinding()]
@@ -10,12 +66,15 @@ function Get-ShellLibrary
     )
     process
     {
-        $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($Name,$false)
-        $r = New-Object ShellLibrary -Property @{
-            Name     = $l.Name
-            TypeName = $l.LibraryType
-            IconReferencePath = $l.IconResourceId.ReferencePath
+        if ( -not ( $Name | Test-ShellLibrary ) )
+        {
+            # the library doesn't exist
+            return
         }
+
+        # the library exists
+        $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($Name,$false)
+        $r = Convert-ShellLibraryObject $l
         $l.Dispose()
         return $r
     }
@@ -32,7 +91,30 @@ function Add-ShellLibrary
     )
     process
     {
-        throw [System.NotImplementedException]::new('New-ShellLibrary')
+        try
+        {
+            $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($Name,$false)
+            $r = Convert-ShellLibraryObject $l
+            return $r
+        }
+        catch [System.Runtime.InteropServices.COMException]
+        {
+            if ( $_.Exception -match 'already exists' )
+            {
+                throw [System.IO.IOException]::new(
+                    "Shell library named $Name already exists.",
+                    $_.Exception
+                )
+            }
+            throw
+        }
+        finally
+        {
+            if ( $null -ne $l )
+            {
+                $l.Dispose()
+            }
+        }
     }
 }
 
--- IntegrationTests/shellLibrary.Tests.ps1
+++ IntegrationTests/shellLibrary.Tests.ps1
@@ -8,41 +8,79 @@ Describe 'set up environment' {
 
 Describe Get-ShellLibrary {
     $guidFrag = [guid]::NewGuid().Guid.Split('-')[0]
-    $libraryName = "MyLibrary-$guidFrag"
-    It 'manually create a real library' {
-        try
-        {
-            $overwrite = $false
-            $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$overwrite)
-            $l.IconResourceId = [Microsoft.WindowsAPICodePack.Shell.IconReference]::new('C:\WINDOWS\system32\imageres.dll,-94')
-            $l.LibraryType = [Microsoft.WindowsAPICodePack.Shell.LibraryFolderType]::Pictures
+    $libraryName = "Get-ShellLibrary-$guidFrag"
+    Context 'library exists' {
+        It 'manually create a real library' {
+            try
+            {
+                $overwrite = $false
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::new($libraryName,$overwrite)
+                $l.IconResourceId = [Microsoft.WindowsAPICodePack.Shell.IconReference]::new('C:\WINDOWS\system32\imageres.dll,-94')
+                $l.LibraryType = [Microsoft.WindowsAPICodePack.Shell.LibraryFolderType]::Pictures
+            }
+            finally
+            {
+                $l.Dispose()
+            }
         }
-        finally
-        {
-            $l.Dispose()
+        It 'returns exactly one ShellLibrary object' {
+            $r = $libraryName | Get-ShellLibrary
+            $r.Count | Should be 1
+            $r.Name | Should be $libraryName
+            $r.GetType() | Should be 'ShellLibrary'
+        }
+        It 'populates type name' {
+            $r = $libraryName | Get-ShellLibrary
+            $r.TypeName | Should be 'Pictures'
+        }
+        It 'populates icon reference path' {
+            $r = $libraryName | Get-ShellLibrary
+            $r.IconReferencePath | Should be 'C:\WINDOWS\system32\imageres.dll,-94'
+        }
+        It 'manually remove the real library' {
+            [System.IO.File]::Delete(($libraryName | Get-ShellLibraryPath))
         }
     }
-    It 'returns exactly one ShellLibrary object' {
-        $r = $libraryName | Get-ShellLibrary
-        $r.Count | Should be 1
-        $r.Name | Should be $libraryName
-        $r.GetType() | Should be 'ShellLibrary'
+    Context 'library does not exist' {
+        It 'returns nothing' {
+            $r = $libraryName | Get-ShellLibrary
+            $r | Should beNullOrEmpty
+        }
     }
-    It 'populates type name' {
-        $r = $libraryName | Get-ShellLibrary
-        $r.TypeName | Should be 'Pictures'
+}
+
+Describe Add-ShellLibrary {
+    $guidFrag = [guid]::NewGuid().Guid.Split('-')[0]
+    $libraryName = "Add-ShellLibrary-$guidFrag"
+    $h = @{}
+    Context 'library doesn''t exist' {
+        It 'returns exactly one ShellLibrary object' {
+            $h.L = $libraryName | Add-ShellLibrary
+            $h.L.Count | Should be 1
+            $h.L.GetType() | Should be 'ShellLibrary'
+        }
+        It 'type name is empty' {
+            $h.L.TypeName | Should beNullOrEmpty
+        }
+        It 'icon reference path is empty' {
+            $h.L.IconReferencePath | Should beNullOrEmpty
+        }
+        It 'confirm that real library was created' {
+            $r = $libraryName | Test-ShellLibrary
+            $r | Should be $true
+            $r = $libraryName | Get-ShellLibrary
+            $r.Name | Should be $libraryName
+        }
     }
-    It 'populates icon reference path' {
-        $r = $libraryName | Get-ShellLibrary
-        $r.IconReferencePath | Should be 'C:\WINDOWS\system32\imageres.dll,-94'
+    Context 'library exists' {
+        It 'throws correct exception' {
+            { $libraryName | Add-ShellLibrary } |
+                Should throw "library named $libraryName already exists"
+        }
     }
-    It 'manually remove the real library' {
-        $librariesPath = [System.IO.Path]::Combine(
-            [System.Environment]::GetFolderPath( [System.Environment+SpecialFolder]::ApplicationData ),
-            [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::LibrariesKnownFolder.RelativePath
-        )
-        $libraryPath = [System.IO.Path]::Combine($librariesPath, $libraryName);
-        $fullLibraryPath = [System.IO.Path]::ChangeExtension($libraryPath, "library-ms")
-        [System.IO.File]::Delete($fullLibraryPath)
+    Context 'cleanup' {
+        It 'manually remove the real library' {
+            [System.IO.File]::Delete(($libraryName | Get-ShellLibraryPath))
+        }
     }
 }
