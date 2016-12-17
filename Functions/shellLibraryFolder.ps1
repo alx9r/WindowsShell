c7bf72e61d0d16fecb2bd2057b975b57b2aef7eb
alx9r <eggy.island9@gmail.com>
2016-12-17 08:04:48 -0800

    implement Test-ShellLibraryFolder

--- Functions/shellLibraryFolder.ps1
+++ Functions/shellLibraryFolder.ps1
@@ -1,3 +1,68 @@
+function Test-ShellLibraryFolder
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   ValueFromPipeline = $true)]
+        [ValidateScript({ $_ | Test-ValidShellLibraryName })]
+        $LibraryName,
+
+        [Parameter(Mandatory = $true,
+                   Position = 1)]
+        [ValidateScript({ $_ | Test-ValidFilePath })]
+        $FolderPath
+    )
+    process
+    {
+        if ( -not ($LibraryName | Test-ShellLibrary) )
+        {
+            # the library doesn't even exist
+            return $false
+        }
+
+        try
+        {
+            # load the library
+            $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$true)
+
+            if ( -not ($FolderPath | Test-Path -PathType Container -ea Stop ) )
+            {
+                # the file system folder does not exist
+                # search through the list of folders
+                foreach ( $folder in $l.GetEnumerator() )
+                {
+                    if ( $folder.Path -eq $FolderPath )
+                    {
+                        # the folder exists
+                        return $true
+                    }
+                }
+
+                # the folder does not exist
+                return $false
+            }
+            
+            # create a reference to the file system folder that the library understands
+            $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($FolderPath)
+
+            if ( -not $l.Contains($f) )
+            {
+                # the folder does not exist
+                return $false
+            }
+        }
+        finally
+        {
+            $l.Dispose()
+        }
+        # new up a [ShellFileSystemFolder] from $FolderPath
+        # test if the library .Contains() the folder
+
+        return $true
+    }
+}
+
 function Add-ShellLibraryFolder
 {
     [CmdletBinding()]
@@ -7,34 +72,17 @@ function Add-ShellLibraryFolder
                    ValueFromPipeline = $true)]
         $LibraryName,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true,
+                   Position = 1)]
+        [ValidateScript({ $_ | Test-ValidFilePath })]
         $FolderPath
     )
     process
     {
+        throw [System.NotImplementedException]::new('Add-ShellLibraryFolder')
         # load the library
         # new up a [ShellFileSystemFolder] from $FolderPath
         # .Add() the folderpath to the library
-    }
-}
-
-function Test-ShellLibraryFolder
-{
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory = $true,
-                   ValueFromPipeline = $true)]
-        $LibraryName,
-
-        [Parameter(Mandatory = $true)]
-        $FolderPath
-    )
-    process
-    {
-        # load the library
-        # new up a [ShellFileSystemFolder] from $FolderPath
-        # test if the library .Contains() the folder
     }
 }
 
@@ -52,6 +100,7 @@ function Remove-ShellLibraryFolder
     )
     process
     {
+        throw [System.NotImplementedException]::new('Remove-ShellLibraryFolder')
         # load the library
         # new up a [ShellFileSystemFolder] from $FolderPath
         # .Remove() it from the library
---
+++ IntegrationTests/shellLibraryFolder.Tests.ps1
@@ -0,0 +1,115 @@
+Import-Module WindowsShell -Force
+
+Describe 'set up environment' {
+    It 'add the Windows API Code Pack assembly' {
+        Add-Type -Path "$PSScriptRoot\..\bin\winapicp\Microsoft.WindowsAPICodePack.Shell.dll"
+    }
+}
+
+Describe Test-ShellLibraryFolder {
+    $guidFrag = [guid]::NewGuid().Guid.Split('-')[0]
+    $libraryName = "Test-ShellLibraryFolder-$guidFrag"
+    $folderPath = Join-Path ([System.IO.Path]::GetTempPath()) "folder-$guidFrag"
+    Context 'set up' {
+        It 'create the library' {
+            Invoke-ProcessShellLibrary Set Present $libraryName
+            $r = Invoke-ProcessShellLibrary Test Present $libraryName
+            $r | Should be $true
+        }
+        It 'create the file system folder' {
+            New-Item $folderPath -ItemType Directory -Force
+            Test-Path $folderPath | Should be $true
+        }
+    }
+    Context 'folder doesn''t exist' {
+        It 'returns false' {
+            $r = $libraryName | Test-ShellLibraryFolder $folderPath
+            $r | Should be $false
+        }
+    }
+    Context 'folder exists' {
+        It 'create the folder' {
+            try
+            {
+                $f = [Microsoft.WindowsAPICodePack.Shell.ShellFileSystemFolder]::FromFolderPath($folderPath)
+                $l = [Microsoft.WindowsAPICodePack.Shell.ShellLibrary]::Load($libraryName,$false)
+                $l.Add($f)
+            }
+            finally
+            {
+                $f.Dispose()
+                $l.Dispose()
+            }
+        }
+        It 'returns true' {
+            $r = $libraryName | Test-ShellLibraryFolder $folderPath
+            $r | Should be $true
+        }
+    }
+    Context 'the folder exists but underlying file system folder does not' {
+        It 'remove the file system folder' {
+            Remove-Item $folderPath
+            Test-Path $folderPath | Should be $false
+        }
+        It 'returns true' {
+            $r = $libraryName | Test-ShellLibraryFolder $folderPath
+            $r | Should be $true
+        }
+    }
+    Context 'the folder exists but the underlying file system path is a file' {
+        It 'create the file at the folder path' {
+            New-Item $folderPath -ItemType File
+            Test-Path $folderPath -PathType Leaf | Should be $true
+        }
+        It 'returns true' {
+            $r = $libraryName | Test-ShellLibraryFolder $folderPath
+            $r | Should be $true
+        }
+    }
+    Context 'the folder does not exist and neither does the file system folder' {
+        It 'remove what''s at FolderPath' {
+            Remove-Item $folderPath
+            Test-Path $folderPath | Should be $false
+        }
+        It 'returns false' {
+            $r = $libraryName | Test-ShellLibraryFolder 'c:\c41f5bdb-478a-40d1-9589-b26cf35e1b33'
+            $r | Should be $false
+        }
+    }
+    Context 'library doesn''t exist' {
+        It 'remove the library' {
+            Invoke-ProcessShellLibrary Set Absent $libraryName
+            $r = Invoke-ProcessShellLibrary Test Absent $libraryName
+            $r | Should be $true
+        }
+        It 'returns false' {
+            $r = $libraryName | Test-ShellLibraryFolder $folderPath
+            $r | Should be $false            
+        }
+    }
+}
+<#
+Describe Add-ShellLibraryFolder {
+    $guidFrag = [guid]::NewGuid().Guid.Split('-')[0]
+    $libraryName = "Add-ShellLibraryFolder-$guidFrag"    
+    Context 'folder doesn''t exist' {
+        It 'create the library' {
+            Invoke-ProcessShellLibrary Set Present $libraryName
+            $r = Invoke-ProcessShellLibrary Test Present $libraryName
+            $r | Should be $true
+        }
+        It 'returns nothing' {
+            $r = $libraryName | Add-ShellLibraryFolder 'folder name'
+            $r | Should beNullOrEmpty
+        }
+    }
+    Context 'folder exists' {}
+    Context 'library doesn''t exist' {}
+    Context 'clean up' {
+        It 'remove the library' {
+            Invoke-ProcessShellLibrary Set Absent $libraryName
+            $r = Invoke-ProcessShellLibrary Test Absent $libraryName
+            $r | Should be $true
+        }
+    }
+}#>
